029d345b21a7fe2dd06c24320737e190b5a34f9e
RektPunk <rektpunk@gmail.com>
2022-10-07 17:37:04 +0900

    feat - add docstring in credential

--- module/credential.py
+++ module/credential.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 
 
 class NeptuneToken:
+    """
+    Set neptune token
+    """
+
     def __init__(self):
         load_dotenv()
         self.neptune_api_token = os.environ["NEPTUNE_API_TOKEN"]
